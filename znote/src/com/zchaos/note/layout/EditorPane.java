cb2987bf029db3040599a509d97c55ac5adfbef8
zhuchx <zhuchx1008@gmail.com>
2012-04-05 15:35:43 +0800

    在tabbed中增加diary标签

--- znote/src/com/zchaos/note/layout/EditorPane.java
+++ znote/src/com/zchaos/note/layout/EditorPane.java
@@ -1,39 +1,32 @@
 package com.zchaos.note.layout;
 
 import java.awt.Color;
-import java.awt.Dimension;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.JTextPane;
 
 import com.zchaos.note.edit.EditTextPane;
 
 public class EditorPane extends JTabbedPane {
 
-	private JTextPane work = new EditTextPane("work");
-	private JTextPane note = new EditTextPane("note");
-	private JTextPane temp = new EditTextPane("temp");
+	private EditTextPane diary = new EditTextPane("diary");
+	private EditTextPane work = new EditTextPane("work");
+	private EditTextPane note = new EditTextPane("note");
+	private EditTextPane temp = new EditTextPane("temp");
 
 	public EditorPane() {
 		super(JTabbedPane.TOP);
 		this.setBackground(Color.RED);
 
-		JScrollPane workScroll = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		this.addTab(work.getName(), workScroll);
-//		workScroll.setRowHeaderView(work);
-		workScroll.setViewportView(work);
-//		work.setPreferredSize(new Dimension(500, 600));
-//		work.setBackground(Color.ORANGE);
-//		workScroll.setBackground(Color.RED);
+		addTab(diary);
+		addTab(work);
+		addTab(note);
+		addTab(temp);
+	}
 
-		JScrollPane noteScroll = new JScrollPane();
-		this.addTab(note.getName(), noteScroll);
-		noteScroll.setViewportView(note);
-
-		JScrollPane tempScroll = new JScrollPane();
-		this.addTab(temp.getName(), tempScroll);
-		tempScroll.setViewportView(temp);
+	private void addTab(EditTextPane pane) {
+		JScrollPane scroll = new JScrollPane();
+		this.addTab(pane.getName(), scroll);
+		scroll.setViewportView(pane);
 	}
 }
